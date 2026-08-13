2ed2bfcc8d5fc2a882929243171c9966cd99eed2
doomzyangek <enzobartolo9030@gmail.com>
2026-08-12 21:16:46 -0300

    criacao do projeto

---
+++ src/main/java/com/example/imagensPecas/ImagensPecasApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagensPecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagensPecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagensPecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imagensPecas/ImagensPecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagensPecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagensPecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
